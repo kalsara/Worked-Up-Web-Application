f716db5e519385d72697c9a260de1fccc1ede7c8
kalsara <kalsaramagamage@gmail.com>
2017-03-08 15:10:52 +0530

    store details in session

--- login.php
+++ login.php
@@ -1,11 +1,4 @@
 <?php
-session_start(); 
-include("db.php");
-//create a variable called $pagename which contains the actual name of the page
-$pagename="Login";
-
-//call in the style sheet called ystylesheet.css to format the page as defined in the style sheet
-echo "<link rel=stylesheet type=text/css href=mystylesheet.css>";
 
 //display window title
 echo "<title>".$pagename."</title>";
@@ -31,7 +24,17 @@ echo"<tr>
      </tr>";
 echo"</form>";
 echo"</table>";
+session_start(); 
+include("db.php");
+//create a variable called $pagename which contains the actual name of the page
+$pagename="Login";
+
+//call in the style sheet called ystylesheet.css to format the page as defined in the style sheet
+echo "<link rel=stylesheet type=text/css href=mystylesheet.css>";
+
 
 //include head layout
 include("footfile.html");
+
+
 ?>
