fdc34b168aa05135be733b24a983327bed127007
kalsara <kalsaramagamage@gmail.com>
2017-03-10 15:17:18 +0530

    databse error handling

--- db.php
+++ db.php
@@ -7,9 +7,6 @@ $conn = mysqli_connect($dbhost, $dbuser, $dbpass) ;
 if (!$conn)
   {
   die('Could not connect: ' . mysqli_error());
-  }else{
-	  
-	  echo "connection success";
   }
 mysqli_select_db($conn,"youraccount_0");
 ?>	
