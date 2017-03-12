6d774fa64f99362447f4069d5e7f5f49552bdec1
kalsara <kalsaramagamage@gmail.com>
2017-03-12 18:28:58 +0530

    delete and config index page

--- index.php
+++ index.php
@@ -8,12 +8,7 @@ $pagename="Index";
 //call in the style sheet called ystylesheet.css to format the page as defined in the style sheet
 echo "<link rel=stylesheet type=text/css href=mystylesheet.css>";
 
-//display window title
-echo "<title>".$pagename."</title>";
-//include head layout 
-include ("headfile.html");
-include ("detectlogin.php");
-echo "<p></p>";
+//d
 //display name of the page and some text
 echo"<div >";
 
