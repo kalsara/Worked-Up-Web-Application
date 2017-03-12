3428e96993f5a566859d89cd6ee6368d3c3ea354
kalsara <kalsaramagamage@gmail.com>
2017-03-12 18:29:26 +0530

    include pages

--- index.php
+++ index.php
@@ -8,7 +8,12 @@ $pagename="Index";
 //call in the style sheet called ystylesheet.css to format the page as defined in the style sheet
 echo "<link rel=stylesheet type=text/css href=mystylesheet.css>";
 
-//d
+//display window title
+echo "<title>".$pagename."</title>";
+//include head layout 
+include ("headfile.html");
+include ("detectlogin.php");
+echo "<p></p>";
 //display name of the page and some text
 echo"<div >";
 
